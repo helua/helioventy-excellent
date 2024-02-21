a9b0d4e41cead86b51b353578cacc9f97f63ee07
helua <helua@users.noreply.github.com>
2024-02-21 14:15:27 +0100

    giftShortcode

--- config/shortcodes/image/index.js
+++ config/shortcodes/image/index.js
@@ -11,23 +11,8 @@ const stringifyAttributes = attributeMap => {
     .join(' ');
 };
 
-const gifShortcode = async (
-  formats = ['gif'],
-) => {
-  const metadata = await Image(src, {
-    formats: [...formats],
-    urlPath: '/assets/images/',
-    outputDir: './dist/assets/images/',
-    sharpOptions: {
-      animated: true
-    },
-    filenameFormat: (id, src, width, format, options) => {
-      const extension = path.extname(src);
-      const name = path.basename(src, extension);
-      return `${name}-${width}w.${format}`;
-    }
-  })
-}
+
+
 const imageShortcode = async (
   src,
   alt = '',
@@ -43,6 +28,69 @@ const imageShortcode = async (
     formats: [...formats],
     urlPath: '/assets/images/',
     outputDir: './dist/assets/images/',
+    filenameFormat: (id, src, width, format, options) => {
+      const extension = path.extname(src);
+      const name = path.basename(src, extension);
+      return `${name}-${width}w.${format}`;
+    }
+  });
+
+  const lowsrc = metadata.jpeg[metadata.jpeg.length - 1];
+
+  // Getting the URL to use
+  let imgSrc = src;
+  if (!imgSrc.startsWith('.')) {
+    const inputPath = this.page.inputPath;
+    const pathParts = inputPath.split('/');
+    pathParts.pop();
+    imgSrc = `${pathParts.join('/')}/${src}`;
+  }
+
+  const imageSources = Object.values(metadata)
+    .map(imageFormat => {
+      return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat
+        .map(entry => entry.srcset)
+        .join(', ')}" sizes="${sizes}">`;
+    })
+    .join('\n');
+
+  const imgageAttributes = stringifyAttributes({
+    src: lowsrc.url,
+    width: lowsrc.width,
+    height: lowsrc.height,
+    alt,
+    loading,
+    decoding: 'async'
+  });
+
+  const imageElement = caption
+    ? `<figure class="flow ${className ? `${className}` : ''}">
+				<picture>
+					${imageSources}
+					<img
+					${imgageAttributes}>
+				</picture>
+				<figcaption>${caption}</figcaption>
+			</figure>`
+    : `<picture class="flow ${className ? `${className}` : ''}">
+				${imageSources}
+				<img
+				${imgageAttributes}>
+			</picture>`;
+
+  return htmlmin.minify(imageElement, {collapseWhitespace: true});
+};
+
+const gifShortcode = async (
+  formats = ['gif'],
+) => {
+  const metadata = await Image(src, {
+    formats: [...formats],
+    urlPath: '/assets/images/',
+    outputDir: './dist/assets/images/',
+    sharpOptions: {
+      animated: true
+    },
     filenameFormat: (id, src, width, format, options) => {
       const extension = path.extname(src);
       const name = path.basename(src, extension);
